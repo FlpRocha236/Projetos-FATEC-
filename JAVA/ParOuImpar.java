7bc2673a97418deb2c41f8d8f37d21c015eb3d26
Felipe Rafael Rocha <109861866+FlpRocha236@users.noreply.github.com>
2025-02-22 20:09:49 -0300

    Mudanças na descrição do código ParOuImpar

--- JAVA/ParOuImpar.java
+++ JAVA/ParOuImpar.java
@@ -1,14 +1,14 @@
 class parImpar {
     public static void main (String args[]){
         int valor = (int)(Math.random() * 100); // Gera um número aleatório
-        // Verifica se o número é par ou ímpar
-        if (valor % 2 == 0)
+        
+        if (valor % 2 == 0) // Verifica se o número é par ou ímpar
         {
-            System.out.println("O número é Par = "+valor);
+            System.out.println("O número é Par = "+valor); // Exibe o resultado Par
         }
         else
         {
-            System.out.println("O número é impar = "+valor);
+            System.out.println("O número é impar = "+valor); // Exibe o resultado impar
         }
         }
     }
